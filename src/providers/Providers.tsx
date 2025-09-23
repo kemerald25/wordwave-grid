07dffa584d550c63499f4f20b4310f4547b5a5a6
Kemerald25 <unityekeoba18@gmail.com>
2025-09-23 01:51:29 +0100

    enabled miniapp

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -24,6 +24,7 @@ export function Providers({ children }: ProvidersProps) {
             theme: 'cyberpunk'
           }
         }}
+        miniapp={{enabled: true}}
       >
         <AuthProvider>
           <TooltipProvider>
