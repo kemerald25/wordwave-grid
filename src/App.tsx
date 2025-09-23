c9e99eeeff0900dd1c014fceaeb2a5f584e3c1dc
Kemerald25 <unityekeoba18@gmail.com>
2025-09-23 03:36:22 +0100

    updated frame ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useMiniKit } from "@coinbase/onchainkit/minikit";
 import { Providers } from "./providers/Providers";
-import { useIsInMiniApp } from "@coinbase/onchainkit/minikit";
 import { AuthGate } from "./components/AuthGate";
 import Index from "./pages/Index";
 import Lobby from "./pages/Lobby";
 import GameDemo from "./pages/GameDemo";
 import NotFound from "./pages/NotFound";
-import { useEffect } from "react";
 
 const AppContent = () => {
-  const { isInMiniApp } = useIsInMiniApp();
-  const location = useLocation();
+  const { isFrameReady, setFrameReady } = useMiniKit();
 
   useEffect(() => {
-    // Log environment info for debugging
-    console.log("App loaded - Location:", location.pathname);
-    console.log("Is in mini app:", isInMiniApp);
-    console.log("User agent:", navigator.userAgent);
-  }, [location.pathname, isInMiniApp]);
+    if (!isFrameReady) {
+      setFrameReady();
+    }
+  }, [setFrameReady, isFrameReady]);
 
   return (
     <BrowserRouter>
-      {isInMiniApp}
       <Routes>
         <Route path="/" element={<Index />} />
         <Route
